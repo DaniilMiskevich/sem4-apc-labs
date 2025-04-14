1880d325312638336a6bae53b64d4af92328d4d6
Daniil Miskevich <daniilmiskevich@gmail.com>
2025-04-14 19:30:12 +0300

    .

--- l5/main.c
+++ l5/main.c
@@ -8,22 +8,22 @@
  ***********************************/
 
 #if (defined __BORLANDC__)
-#  include <conio.h>
-#  include <dos.h>
+#include <conio.h>
+#include <dos.h>
 #else
 
-#  include "include/borland/conio.h"
-#  include "include/borland/dos.h"
+#include "include/borland/conio.h"
+#include "include/borland/dos.h"
 
-#  define _dos_getvect(ADDR_)           ((void *)0)
-#  define _dos_setvect(ADDR_, HANDLER_) ((void)0)
+#define _dos_getvect(ADDR_) ((void *)0)
+#define _dos_setvect(ADDR_, HANDLER_) ((void)0)
 
-#  define FP_SEG(FP_) (*(FP_ + 1))
-#  define FP_OFF(FP_) (*FP_)
+#define FP_SEG(FP_) (*(FP_ + 1))
+#define FP_OFF(FP_) (*FP_)
 
-#  define _SP (0)
-#  define _DS ((void *)0)
-#  define _CS ((void *)0)
+#define _SP (0)
+#define _DS ((void *)0)
+#define _CS ((void *)0)
 #endif
 
 /******************
@@ -31,7 +31,7 @@
  ******************/
 
 #define elsizeof(ARR_) (sizeof(*ARR_))
-#define lenof(ARR_)    (sizeof(ARR_) / elsizeof(ARR_))
+#define lenof(ARR_) (sizeof(ARR_) / elsizeof(ARR_))
 
 /************
  ** consts **
@@ -50,49 +50,61 @@
  **********/
 
 static unsigned volatile do_run = 0;
-static unsigned volatile was_send = 0, was_send_success = 0;
+static int volatile send_result = 0;
 
 static void interrupt (*old_intr_handler)(void) = NULL;
 static void interrupt new_intr_handler(void) {
-    if (was_send) {
-        unsigned ret_code = inportb(0x60);
-        was_send_success = ret_code == 0xFA;
-        was_send = 0;
+  unsigned ret_code = inportb(0x60);
+  switch (ret_code) {
+  case 0xFA:
+    send_result = 1;
+    break;
+  case 0xFE:
+    send_result = -1;
+    break;
 
-        printf("0x%X\n", ret_code);
-    } else {
-        unsigned scancode = inportb(0x60);
-        if (scancode == SCANCODE_ESC) do_run = 0;
-    }
+  case SCANCODE_ESC:
+    do_run = 0;
+    break;
+  }
 
-    if (old_intr_handler) old_intr_handler();
-    disable(), outportb(0xA0, 0x20), outportb(0x20, 0x20), enable();
+  printf("0x%X\n", ret_code);
+
+  if (old_intr_handler)
+    old_intr_handler();
+  disable(), outportb(0xA0, 0x20), outportb(0x20, 0x20), enable();
 }
 
 static void intr_handler_set(void) {
-    assert(old_intr_handler == NULL);
-    old_intr_handler = _dos_getvect(0x09);
-    _dos_setvect(0x09, new_intr_handler);
+  assert(old_intr_handler == NULL);
+  old_intr_handler = _dos_getvect(0x09);
+  _dos_setvect(0x09, new_intr_handler);
 }
 static void intr_handler_restore(void) {
-    _dos_setvect(0x09, old_intr_handler), old_intr_handler = NULL;
+  _dos_setvect(0x09, old_intr_handler), old_intr_handler = NULL;
 }
 
 static void kb_send(unsigned char cmd) {
-    unsigned kb_send_attempt_count = 0;
-    for (kb_send_attempt_count = 0;
-         kb_send_attempt_count < MAX_KB_SEND_ATTEMPT_COUNT;
-         kb_send_attempt_count++) {
-        while (inportb(0x64) & (1 << 1));
-        outportb(0x60, cmd);
+  unsigned kb_send_attempt_count = 0;
+  while (kb_send_attempt_count < MAX_KB_SEND_ATTEMPT_COUNT) {
+    while (inportb(0x64) & (1 << 1))
+      ;
+    outportb(0x60, cmd);
+    while (!send_result)
+      ;
 
-        was_send_success = 0, was_send = 1;
-        while (was_send);
-        if (was_send_success) return;
+    switch (send_result) {
+    case -1:
+      send_result = 0, kb_send_attempt_count++;
+      break;
+    case 1:
+      send_result = 0;
+      return;
     }
+  }
 
-    printf("error sending byte. exiting...\n");
-    intr_handler_restore(), exit(1);
+  printf("error sending byte. exiting...\n");
+  intr_handler_restore(), exit(1);
 }
 static void kb_set_leds(unsigned leds) { kb_send(0xED), kb_send(leds); }
 
@@ -101,16 +113,16 @@ static void kb_set_leds(unsigned leds) { kb_send(0xED), kb_send(leds); }
  **********/
 
 int main() {
-    unsigned i = 0;
+  unsigned i = 0;
 
-    intr_handler_set();
+  intr_handler_set();
 
-    do_run = 1;
-    while (do_run) {
-        kb_set_leds(i++ % 3);
-        delay(300);
-    }
+  do_run = 1;
+  while (do_run) {
+    kb_set_leds(i++ & 7);
+    delay(300);
+  }
 
-    intr_handler_restore();
-    return 0;
+  intr_handler_restore();
+  return 0;
 }
